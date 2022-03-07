1d602ac6588f913910a834976008ffe5fe4802ae
dboyce <dallin.boyce@revisionsystems.net>
2022-03-07 00:36:12 -0700

    Adding base

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+from alpine:latest
+run apk add --no-cache --upgrade bash
+run apk update
+run apk add docker
+# run addgroup username docker
+run apk add --update nodejs npm
+cmd ["/bin/bash"]
---
+++ ci.js
@@ -0,0 +1,16 @@
+const utc = new Date();
+const fs = require('fs');
+
+const path = "package.json"
+const versionPath = ".version";
+
+if (fs.existsSync(path)) {
+    const fileBuffer = fs.readFileSync(path);
+    const jsonRaw = fileBuffer.toString('utf8');
+    const packageJson = JSON.parse(jsonRaw);
+    const versionToWrite = `${packageJson.version}.${utc.getFullYear()}.${utc.getMonth() + 1}.${utc.getDate()}.${utc.getHours()}.${utc.getMinutes()}.${utc.getSeconds()}`;
+    fs.writeFileSync(versionPath, versionToWrite);
+} else {
+   console.error("No package.json exists.");
+   process.exit(1);
+}
