075dbe3cefd976909f42585fc41d1bdf149148be
dboyce <dallin.boyce@revisionsystems.net>
2022-03-07 00:38:47 -0700

    Adding console log for version

--- ci.js
+++ ci.js
@@ -10,6 +10,7 @@ if (fs.existsSync(path)) {
     const packageJson = JSON.parse(jsonRaw);
     const versionToWrite = `${packageJson.version}.${utc.getFullYear()}.${utc.getMonth() + 1}.${utc.getDate()}.${utc.getHours()}.${utc.getMinutes()}.${utc.getSeconds()}`;
     fs.writeFileSync(versionPath, versionToWrite);
+    console.log(`Success: New version is -> ${versionToWrite}`)
 } else {
    console.error("No package.json exists.");
    process.exit(1);
